8085dfc061ff1ea6d5b0d1c5c907ce2045837c8a
Joaquinmes18 <joaquineliassoriaa@gmail.com>
2025-10-05 19:44:46 -0400

    Add IEventService contract

---
+++ Services/IEventService.cs
@@ -0,0 +1,13 @@
+﻿using apiwithdb.Models;
+using apiwithdb.Models.dtos;
+
+namespace apiwithdb.Services
+{
+    public interface IEventService
+    {
+        IEnumerable<Event> GetAll();
+        Event? GetById(Guid id);
+        Event Create(CreateEventDto dto);
+        bool Delete(Guid id);
+    }
+}
