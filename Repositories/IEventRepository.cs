8541c86ef2ebaead1cf28a26ad51485ddc866bfa
Joaquinmes18 <joaquineliassoriaa@gmail.com>
2025-10-05 19:43:29 -0400

    Add IEventRepository contract

---
+++ Repositories/IEventRepository.cs
@@ -0,0 +1,12 @@
+﻿using apiwithdb.Models;
+
+namespace apiwithdb.Repositories
+{
+    public interface IEventRepository
+    {
+        IEnumerable<Event> GetAll();
+        Event? GetById(Guid id);
+        void Add(Event ev);
+        void Delete(Guid id);
+    }
+}
