a6e019898077c6a956c4ef03384b827cba353b78
Joaquinmes18 <joaquineliassoriaa@gmail.com>
2025-10-05 19:44:58 -0400

    Wire up DI and minimal pipeline in Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using apiwithdb.Repositories;
+using apiwithdb.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// DI para Event (lista en memoria)
+builder.Services.AddSingleton<IEventRepository, EventRepository>();
+builder.Services.AddScoped<IEventService, EventService>();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
